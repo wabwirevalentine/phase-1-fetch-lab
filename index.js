3f1d2675b3c39527237527bdf9ac1f7e165140fe
wabwirevalentine <levaksmaita@gmail.com>
2025-04-17 08:53:12 +0300

    render books

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch("https://anapioficeandfire.com/api/books")
+  return fetch("https://anapioficeandfire.com/api/books")
     .then((resp) => resp.json())
     .then((json) => renderBooks(json));
 }
